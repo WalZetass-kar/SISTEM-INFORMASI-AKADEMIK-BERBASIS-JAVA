10a7aa468aa72f488a67506109e46cca9bab6363
WalZetass-kar <mihwalmaulana09@gmail.com>
2026-06-05 03:55:03 -0400

    update kel 1

--- backend/routes/akademikRoutes.js
+++ backend/routes/akademikRoutes.js
@@ -1,21 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const akademikController = require('../controllers/akademikController');
-const { verifyToken, isAdmin } = require('../middleware/auth');
+const { verifyToken, isAdmin, isAdminOrOwner } = require('../middleware/auth');
 
 router.use(verifyToken);
-router.use(isAdmin);
 
-router.get('/settings', akademikController.getSettings);
+// --- Modul Pengaturan Akademik (Admin Only) ---
+router.get('/akademik/settings', isAdmin, akademikController.getSettings);
+router.post('/akademik/tahun-ajaran', isAdmin, akademikController.createTahunAjaran);
+router.put('/akademik/tahun-ajaran/:id', isAdmin, akademikController.updateTahunAjaran);
+router.delete('/akademik/tahun-ajaran/:id', isAdmin, akademikController.deleteTahunAjaran);
 
-router.post('/tahun-ajaran', akademikController.createTahunAjaran);
-router.put('/tahun-ajaran/:id', akademikController.updateTahunAjaran);
-router.delete('/tahun-ajaran/:id', akademikController.deleteTahunAjaran);
+router.post('/akademik/mata-kuliah', isAdmin, akademikController.createMataKuliah);
+router.put('/akademik/mata-kuliah/:kode_mk', isAdmin, akademikController.updateMataKuliah);
+router.delete('/akademik/mata-kuliah/:kode_mk', isAdmin, akademikController.deleteMataKuliah);
 
-router.post('/mata-kuliah', akademikController.createMataKuliah);
-router.put('/mata-kuliah/:kode_mk', akademikController.updateMataKuliah);
-router.delete('/mata-kuliah/:kode_mk', akademikController.deleteMataKuliah);
+router.put('/akademik/bobot-nilai', isAdmin, akademikController.updateBobotNilai);
 
-router.put('/bobot-nilai', akademikController.updateBobotNilai);
+// --- Modul KRS & Jadwal Kuliah ---
+router.get('/matakuliah', akademikController.getMatakuliah);
+router.post('/matakuliah', isAdmin, akademikController.createMatakuliah);
+
+router.get('/krs', akademikController.getKrs);
+router.post('/krs', isAdminOrOwner, akademikController.createKrs);
+
+router.get('/jadwal', akademikController.getJadwal);
+router.post('/jadwal', isAdmin, akademikController.createJadwal);
 
 module.exports = router;
